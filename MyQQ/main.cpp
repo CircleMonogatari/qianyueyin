5ff709c735b7ccc56dfbd28a2ff7777c3bd413da
CircleMonogatari <circlemono@outlook.com>
2020-03-15 14:37:50 +0800

    upload

--- MyQQ/main.cpp
+++ MyQQ/main.cpp
@@ -1,11 +1,14 @@
 #include "widget.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+    w.setWindowIcon(QIcon(":/Image/qq.png"));
+
     w.show();
     return a.exec();
 }
